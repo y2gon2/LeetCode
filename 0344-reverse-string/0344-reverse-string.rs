c958b6b9e0037c0fd56821ae5b350e78d3e0a6aa
y2gon2 <91047596+y2gon2@users.noreply.github.com>
2023-06-15 13:58:59 +0900

    Time: 16 ms (38.56%), Space: 5.5 MB (64.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.rs
+++ 0344-reverse-string/0344-reverse-string.rs
@@ -1,11 +1,6 @@
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        let len = s.len();
-        for i in 0..len / 2 {
-            let temp = s[i];
-            s[i] = s[len - 1 - i];
-            s[len - 1 - i] = temp;
-        }
+        *s = s.iter().rev().map(|c| *c).collect(); 
         
     }
 }
